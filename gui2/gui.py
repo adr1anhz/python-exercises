d5dd8d728d5d9a87152248ad2b4911ccdd694486
adr1anhz <addriaan@proton.me>
2025-06-05 09:25:10 +0200

    Added gui2

---
+++ gui2/gui.py
@@ -0,0 +1,11 @@
+#import FreePySimpleGUI as sg
+import FreeSimpleGUI as sg
+
+label = sg.Text("Type in a to-do")
+input_box = sg.InputText(tooltip="Enter todo")
+add_button = sg.Button("Add")
+
+window = sg.Window('My To-Do app', layout=[[label], [input_box], [add_button]])
+window.read()
+print("hello")
+window.close()
