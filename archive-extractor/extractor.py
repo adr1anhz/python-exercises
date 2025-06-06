7ee4a9aff891cc76fa8c168b6c27ab42f1bbc856
adr1anhz <addriaan@proton.me>
2025-06-06 18:17:33 +0200

    Added backend to extractor.py

--- archive-extractor/extractor.py
+++ archive-extractor/extractor.py
@@ -1,4 +1,5 @@
 import PySimpleGUI as sg
+from zip_extractor import extract_archive
 
 
 sg.theme("Black")
@@ -21,6 +22,14 @@ window = sg.Window("Archive Extractor",
                    layout=[[label1, input1, choose_button1],
                            [label2, input2, choose_button2],
                            [extract_button, output_label]])
+while True:
+        event, values = window.read()
+        print(event, values)
+        archivepath = values["archive"]
+        dest_dir = values["folder"]
+        extract_archive(archivepath, dest_dir)
+        window["output"].update(value="Extraction Completed")
+
 
 
 window.read()
