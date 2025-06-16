274972592ea299724f337c1ab5458ec8536a4a55
adr1anhz <addriaan@proton.me>
2025-06-16 13:15:05 +0200

    Add app description, image and links

--- portfolio/main.py
+++ portfolio/main.py
@@ -13,9 +13,9 @@ with col1:
 
 
 with col2:
-    st.title("Adrian Adrian")
+    st.title("Adrian")
     content = """
-    Hi, this is personal portfolio here we can write resume about us
+    Hi, this is personal portfolio below we can put example projects which we did in the past
     """
     st.info(content)
 
@@ -26,9 +26,20 @@ Below you can find some of the apps i have built in Python. Feel free to contact
 st.write(content2)
 
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pandas.read_csv("data.csv", sep=";")
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
+
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
