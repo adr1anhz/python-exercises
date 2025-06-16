e1581bea62b54da871c784f426110c90df0001ad
adr1anhz <addriaan@proton.me>
2025-06-16 13:04:27 +0200

    Render the app titles in two columns

--- portfolio/main.py
+++ portfolio/main.py
@@ -1,6 +1,9 @@
 import streamlit as st
+import pandas
+
 
 st.set_page_config(layout="wide")
+
 
 col1, col2 = st.columns(2)
 
@@ -15,3 +18,17 @@ with col2:
     Hi, this is personal portfolio here we can write resume about us
     """
     st.info(content)
+
+
+content2 = """
+Below you can find some of the apps i have built in Python. Feel free to contact me!
+"""
+st.write(content2)
+
+
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv("data.csv", sep=";")
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
