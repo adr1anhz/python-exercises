24b542b2d91a8965bcbe6e91073524c443ef2ab4
adr1anhz <addriaan@proton.me>
2025-06-17 15:26:18 +0200

    Added message and sending function

--- portfolio/pages/Contact_Us.py
+++ portfolio/pages/Contact_Us.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from send_email import send_email
 
 
 st.header("Contact Me")
@@ -6,8 +7,14 @@ st.header("Contact Me")
 
 with st.form(key="email_forms"):
     user_email = st.text_input("Your Email Address")
-    message = st.text_area("Your message")
+    raw_message = st.text_area("Your message")
+    message = f"""\
+    Subject: New email from {user_email}
+    From: {user_email}
+    {raw_message}
+    """
     button = st.form_submit_button("Submit")
+    print(button)
     if button:
-        message = message + user_email
-        print(button)
+        send_email(message)
+        st.info("Your email was sent successfully")
