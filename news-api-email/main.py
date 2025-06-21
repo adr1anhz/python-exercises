7fa3421e973f65f11bd7bb29a4429f0aaec527ff
adr1anhz <addriaan@proton.me>
2025-06-21 12:10:12 +0200

    Added to send a email with news

--- news-api-email/main.py
+++ news-api-email/main.py
@@ -1,4 +1,5 @@
 import requests
+from send_email import send_email
 
 
 api_key = "619a1e89c25e494cb45933ef77e16a7f"
@@ -6,9 +7,12 @@ url = "https://newsapi.org/v2/everything?q=tesla&sortBy=publishedAt&apiKey=619a1
 
 request = requests.get(url)
 content = request.json()
+
+body = ""
 for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+    if article["title"] is not None:
+        body = body + article["title"] + "\n" + str(article["description"]) + 2*"\n"
 
-
+body = body.encode("utf-8")
+send_email(message=body)
 # 619a1e89c25e494cb45933ef77e16a7f
