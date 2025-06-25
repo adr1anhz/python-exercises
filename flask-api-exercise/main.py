7669b9ae1566366722c1bf7bff21458e952c3d4f
adr1anhz <addriaan@proton.me>
2025-06-25 10:09:55 +0200

    Changed to instead extract word from API requests to extract from csv file

--- flask-api-exercise/main.py
+++ flask-api-exercise/main.py
@@ -1,23 +1,21 @@
 from flask import Flask, render_template
-import requests
+import pandas as pd
 
 app = Flask(__name__)
 
+df = pd.read_csv("dictionary.csv")
 
-@app.route("/")
+@app.route('/')
 def home():
     return render_template("home.html")
 
 
+@app.route('/api/v1/<word>/')
+def api(word):
+    definition = df.loc[df["word"] == word]['definition'].squeeze()
+    result_dictionary = {'word': word, 'definition': definition}
+    return result_dictionary
 
-@app.route("/api/v1/<word>")
-def get_definition(word):
-    info = f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}"
-    f = requests.get(info)
-    f2 = f.json()
-    definition = f2[0]['meanings'][0]['definitions'][0]['definition']
-    return {"definition": definition,
-            "word": word}
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=5001)
