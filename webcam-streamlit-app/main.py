ea89d9550389dd4cb7a1c8937a02b64f4e2bd106
adr1anhz <addriaan@proton.me>
2025-07-01 10:30:00 +0200

    Add definition of sending email

--- webcam-streamlit-app/main.py
+++ webcam-streamlit-app/main.py
@@ -1,5 +1,6 @@
 import cv2
 import streamlit as st
+from datetime import datetime
 
 st.title("Motion Detector")
 start = st.button("Start Camera")
@@ -11,9 +12,14 @@ if start:
     while True:
         check, frame = camera.read()
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        data = datetime.today().strftime('%A')
+        data2 = datetime.today().strftime('%H:%M:%S')
 
-        cv2.putText(img=frame, text="Hello", org=(50, 50),
-                    fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=2, color=(20, 100, 200),
+        cv2.putText(img=frame, text=data, org=(20, 35),
+                    fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=2, color=(255, 255, 255),
+                    thickness=2, lineType=cv2.LINE_AA)
+        cv2.putText(img=frame, text=data2, org=(20, 70),
+                    fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=2, color=(255, 0, 0),
                     thickness=2, lineType=cv2.LINE_AA)
         
         streamlit_image.image(frame)
