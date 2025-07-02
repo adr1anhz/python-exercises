429256f16ee6aa11490965c9959f8f57135459bc
adr1anhz <addriaan@proton.me>
2025-07-02 11:15:38 +0200

    Added definition to extract css id

--- web-scraping1/main.py
+++ web-scraping1/main.py
@@ -1,9 +1,12 @@
 import requests
 import selectorlib
 
+
 URL = "https://programmer100.pythonanywhere.com/tours/"
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+
 
 def scrape(url):
     """Scrape the page source from the URL"""
@@ -12,5 +15,13 @@ def scrape(url):
     return source
 
 
+def extract(source):
+    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+    value = extractor.extract(source)["tours"]
+    return value
+
+
 if __name__ == "__main__":
-    print(scrape(URL))
+    scraped = scrape(URL)
+    extracted = extract(scraped)
+    print(extracted)
