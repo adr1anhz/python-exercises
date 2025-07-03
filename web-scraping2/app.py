b11142bdc2bea0babc16ec3f8277d5a8bcffc584
adr1anhz <addriaan@proton.me>
2025-07-03 15:21:26 +0200

    Add instead of reading from logs.txt to read from database and show that on chart in streamlit

--- web-scraping2/app.py
+++ web-scraping2/app.py
@@ -1,8 +1,19 @@
 import streamlit as st
 import plotly.express as px
-import pandas
+import sqlite3
+import pandas as pd
 
-df = pandas.read_csv("logs.txt")
+connection = sqlite3.connect("data")
+
+
+def read():
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM events")
+    rows = cursor.fetchall()
+    df = pd.DataFrame(rows, columns=["date", "temperature"])
+    return df
+
+df = read()
 
 figure = px.line(x=df["date"], y=df["temperature"], labels={"x": "Date", "y": "Temperature (C)"})
 
