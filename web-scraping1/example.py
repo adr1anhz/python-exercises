e6e6881283beb416f85b7675fc0d0dcedd7e0111
adr1anhz <addriaan@proton.me>
2025-07-03 14:31:08 +0200

    Add query all data

--- web-scraping1/example.py
+++ web-scraping1/example.py
@@ -24,3 +24,8 @@ print(rows)
 #              ('Hens', 'Hen City', '2088.10.17')]
 # cursor.executemany("INSERT INTO events VALUES(?,?,?)", new_rows)
 # connection.commit()
+
+# Query all data
+cursor.execute("SELECT * FROM events")
+rows = cursor.fetchall()
+print(rows)
