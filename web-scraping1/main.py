9098db5b07f0ff11089d17b2c98b130f84e855fc
adr1anhz <addriaan@proton.me>
2025-07-03 15:01:08 +0200

    Add storing and read in SQL

--- web-scraping1/main.py
+++ web-scraping1/main.py
@@ -2,10 +2,18 @@ import requests
 import selectorlib
 from emailing import send_email
 import time
+import sqlite3
+
+"INSERT INTO events VALUES('Tigers', 'Tiger City', '2088.10.14')"
+"SELECT * FROM events WHERE date='2088.10.18'"
+"DELETE FROM events WHERE band='Tigers'"
 
 URL = "https://programmer100.pythonanywhere.com/tours/"
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+
+connection = sqlite3.connect("data")
 
 
 
@@ -23,12 +31,20 @@ def extract(source):
 
 
 def store(extracted):
-    with open("logs.txt", "a") as file:
-        file.write(extracted + "\n")
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    cursor = connection.cursor()
+    cursor.execute()
 
 def read(extracted):
-     with open("logs.txt", "r") as file:
-          return file.read()
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    band, city, date = row
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?", (band, city, date))
+    rows = cursor.fetchall()
+    print(rows)
+    return rows
 
 if __name__ == "__main__":
     while True:
@@ -37,7 +53,8 @@ if __name__ == "__main__":
         print(extracted)
         content = read(extracted)
         if extracted != "No upcoming tours":
-            if extracted not in content:
+            row = read(extracted)
+            if not row:
                 store(extracted)
                 send_email(message=f"Hey, new event was found! - {extracted}")
         time.sleep(2)
