d941d636372174a40999f5bf8c2f4a0ff5988dd4
adr1anhz <addriaan@proton.me>
2025-07-05 13:22:54 +0200

    Add ReservationTicket logic methods

--- hotel-booking/main.py
+++ hotel-booking/main.py
@@ -5,6 +5,7 @@ df = pandas.read_csv("hotels.csv", dtype={"id": str})
 class Hotel:
     def __init__(self, hotel_id):
         self.hotel_id = hotel_id
+        self.name = df.loc[df["id"] == self.hotel_id, "name"].squeeze()
 
     def book(self):
         """Book a hotel by changing its availability to no"""
@@ -21,11 +22,18 @@ class Hotel:
 
 
 class ReservationTicket:
-    def __init__(self, customer_ame, hotel_object):
-        pass
+    def __init__(self, customer_name, hotel_object):
+        self.customer_name = customer_name
+        self.hotel = hotel_object
 
     def generate(self):
-        pass
+        content = f"""
+        Thank you for your reservation!
+        Here are you booking data:
+        Name: {self.customer_name}
+        Hotel Name: {self.hotel.name}
+        """
+        return content
 
 
 print(df)
@@ -34,7 +42,7 @@ hotel = Hotel(hotel_ID)
 if hotel.available():
     hotel.book()
     name = input("Enter your name: ")
-    reservation_ticket = ReservationTicket(name, hotel)
+    reservation_ticket = ReservationTicket(customer_name=name, hotel_object=hotel)
     print(reservation_ticket.generate())
 else:
     print("Hotel is not free.")
